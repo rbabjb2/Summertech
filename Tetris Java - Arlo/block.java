adda3cc6274afa06e4d972de487ead748c1d708d
rbabjb2 <sblbrbabjb2@gmail.com>
2025-06-30 12:43:45 -0400

    Tetris 30/6/25

---
+++ Tetris Java - Arlo/block.java
@@ -0,0 +1,58 @@
+public class block {
+    private String blockType;
+    public int posX;
+    public int posY;
+    private int screenSizeX;
+    private int screenSizeY;
+
+    public void reset(String type) {
+        this.blockType = type;
+        if (this.blockType == "square") {
+            this.posX = screenSizeX / 2;
+            this.posY = 0;
+        } else if (this.blockType == "line") {
+            this.posX = screenSizeX / 2;
+            this.posY = 0; 
+        } else if (this.blockType.equalsIgnoreCase("l")) {
+
+        }else if (this.blockType == "cube") {
+
+        } else if (this.blockType.equalsIgnoreCase("j")) {
+
+        } else if (this.blockType.equalsIgnoreCase("s")) {
+
+        } else if (this.blockType.equalsIgnoreCase("z")) {
+
+        }
+    }
+    //needs wall detection
+    public void move(String direction) {
+        if (direction == "down") {
+            this.posY++;
+        } else if (direction == "left") {
+            this.posX--;
+        } else if (direction == "right") {
+            this.posX++;
+        } else {
+            System.out.println("Error: Please check the move script and where you call it.");
+        }
+    }
+    public int[] getPos() {
+        int[] pos = new int[2];
+        pos[0] = this.posX;
+        pos[1] = this.posY;
+        return pos;
+    }
+
+    public void screenSizeSet(int screenSizeX, int screenSizeY) {
+        this.screenSizeX = screenSizeX;
+        this.screenSizeY = screenSizeY;
+        this.reset("square");
+        }
+
+    public String getType() {
+        return this.blockType;
+    }
+
+
+}
